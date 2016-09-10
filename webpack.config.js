9e896a0b4816e0a808e99d487cacc39494c406a3
Jo Jangmi <wkdal720@gmail.com>
2016-09-10 07:46:40 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  context: __dirname,
+  entry: "./lib/watercolor.js",
+  output: {
+    path: "./lib",
+    publicPath: "/lib/",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-maps'
+};
